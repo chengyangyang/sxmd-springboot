1044fdabb0d7454c9a15c1d0ccb4d67be467b27a
cy <390518881@qq.com>
2019-07-23 08:38:20 +0800

    修改文件在liunx下的路径，打包跳过测试

--- generator/src/main/java/com/sxmd/config/FtlConfig.java
+++ generator/src/main/java/com/sxmd/config/FtlConfig.java
@@ -17,19 +17,23 @@ import java.util.Objects;
 public class FtlConfig {
 
     private static Map map = new HashMap<String, FtlEntity>();
+    /**
+     * 基本的文件夹路径  E:\\
+     */
+    private static String BASE_PATH = "/file/generator";
 
     static {
-        map.put("hollysys/entity.ftl",new FtlEntity("hollysys/entity.ftl","E:\\","{0}.java"));
-        map.put("hollysys/AddModel.ftl",new FtlEntity("hollysys/AddModel.ftl","E:\\","{0}AddModel.java"));
-        map.put("hollysys/EditModel.ftl",new FtlEntity("hollysys/EditModel.ftl","E:\\","{0}EditModel.java"));
-        map.put("hollysys/ListModel.ftl",new FtlEntity("hollysys/ListModel.ftl","E:\\","{0}ListModel.java"));
-        map.put("hollysys/Model.ftl",new FtlEntity("hollysys/Model.ftl","E:\\","{0}Model.java"));
+        map.put("hollysys/entity.ftl",new FtlEntity("hollysys/entity.ftl",BASE_PATH,"{0}.java"));
+        map.put("hollysys/AddModel.ftl",new FtlEntity("hollysys/AddModel.ftl",BASE_PATH,"{0}AddModel.java"));
+        map.put("hollysys/EditModel.ftl",new FtlEntity("hollysys/EditModel.ftl",BASE_PATH,"{0}EditModel.java"));
+        map.put("hollysys/ListModel.ftl",new FtlEntity("hollysys/ListModel.ftl",BASE_PATH,"{0}ListModel.java"));
+        map.put("hollysys/Model.ftl",new FtlEntity("hollysys/Model.ftl",BASE_PATH,"{0}Model.java"));
 
-        map.put("hollysys/dao.ftl",new FtlEntity("hollysys/dao.ftl","E:\\","{0}Dao.java"));
-        map.put("hollysys/controller.ftl",new FtlEntity("hollysys/controller.ftl","E:\\","{0}Controller.java"));
-        map.put("hollysys/service.ftl",new FtlEntity("hollysys/service.ftl","E:\\","{0}Service.java"));
-        map.put("hollysys/serviceimpl.ftl",new FtlEntity("hollysys/serviceimpl.ftl","E:\\","{0}ServiceImpl.java"));
-        map.put("hollysys/xml.ftl",new FtlEntity("hollysys/xml.ftl","E:\\","{0}Mapper.java"));
+        map.put("hollysys/dao.ftl",new FtlEntity("hollysys/dao.ftl",BASE_PATH,"{0}Dao.java"));
+        map.put("hollysys/controller.ftl",new FtlEntity("hollysys/controller.ftl",BASE_PATH,"{0}Controller.java"));
+        map.put("hollysys/service.ftl",new FtlEntity("hollysys/service.ftl",BASE_PATH,"{0}Service.java"));
+        map.put("hollysys/serviceimpl.ftl",new FtlEntity("hollysys/serviceimpl.ftl",BASE_PATH,"{0}ServiceImpl.java"));
+        map.put("hollysys/xml.ftl",new FtlEntity("hollysys/xml.ftl",BASE_PATH,"{0}Mapper.java"));
     }
 
 
